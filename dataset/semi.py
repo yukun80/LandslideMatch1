e642cc5b9ac615c5bdb4601cad6a3026243a7d0d
yukun80 <fanyukun22@mails.ucas.ac.cn>
2024-05-15 09:57:49 +0800

    添加注释

--- dataset/semi.py
+++ dataset/semi.py
@@ -14,61 +14,79 @@ from torchvision import transforms
 
 class SemiDataset(Dataset):
     def __init__(self, name, root, mode, size=None, id_path=None, nsample=None):
+        """
+        - name: 数据集名称
+        - root: 数据集根目录
+        - mode: 数据集模式(train_l、train_u、val)
+        - size: 图片尺寸
+        - id_path: 数据集ID文件路径
+        - nsample: 样本数量(仅在mode为train_l时有效)
+        """
         self.name = name
         self.root = root
         self.mode = mode
         self.size = size
 
-        if mode == 'train_l' or mode == 'train_u':
-            with open(id_path, 'r') as f:
-                self.ids = f.read().splitlines()
-            if mode == 'train_l' and nsample is not None:
-                self.ids *= math.ceil(nsample / len(self.ids))
-                self.ids = self.ids[:nsample]
-        else:
-            with open('splits/%s/val.txt' % name, 'r') as f:
+        if mode == "train_l" or mode == "train_u":  # 如果是训练模式
+            with open(id_path, "r") as f:  # 从指定的路径读取ID
+                self.ids = f.read().splitlines()  # 读取所有行
+            if mode == "train_l" and nsample is not None:  # 如果是有标签的训练模式且指定了样本数
+                self.ids *= math.ceil(nsample / len(self.ids))  # 扩展列表，确保足够数量的样本
+                self.ids = self.ids[:nsample]  # 截取指定数量的样本
+        else:  # 验证模式
+            with open("splits/%s/val.txt" % name, "r") as f:  # 从验证分割文件读取ID
                 self.ids = f.read().splitlines()
 
     def __getitem__(self, item):
+        """
+        根据数据集中的索引 (item) 检索图像及其相应的掩码。
+        图像被打开并转换为RGB；掩码被加载并转换为数组，然后返回图像以实现处理兼容性。
+        """
         id = self.ids[item]
-        img = Image.open(os.path.join(self.root, id.split(' ')[0])).convert('RGB')
-        mask = Image.fromarray(np.array(Image.open(os.path.join(self.root, id.split(' ')[1]))))
-
-        if self.mode == 'val':
-            img, mask = normalize(img, mask)
+        img = Image.open(os.path.join(self.root, id.split(" ")[0])).convert("RGB")
+        mask = Image.fromarray(np.array(Image.open(os.path.join(self.root, id.split(" ")[1]))))
+        """
+        如果是验证模式，则对图像及掩码进行归一化处理后返回；
+        """
+        if self.mode == "val":
+            img, mask = normalize(img, mask)  # 应用归一化
             return img, mask, id
-
-        img, mask = resize(img, mask, (0.5, 2.0))
-        ignore_value = 254 if self.mode == 'train_u' else 255
-        img, mask = crop(img, mask, self.size, ignore_value)
-        img, mask = hflip(img, mask, p=0.5)
-
-        if self.mode == 'train_l':
+        """对输入图像应用裁剪和翻转等弱扰动"""
+        img, mask = resize(img, mask, (0.5, 2.0))  # 调整大小
+        ignore_value = 254 if self.mode == "train_u" else 255  # 设置忽略值
+        img, mask = crop(img, mask, self.size, ignore_value)  # 裁剪
+        img, mask = hflip(img, mask, p=0.5)  # 水平翻转
+        """
+        如果是有标签的训练模式，则对图像及掩码进行归一化处理后返回；
+        """
+        if self.mode == "train_l":
             return normalize(img, mask)
 
-        img_w, img_s1, img_s2 = deepcopy(img), deepcopy(img), deepcopy(img)
-
-        if random.random() < 0.8:
+        """
+        对于无标签训练模式，图像会经历多次变换：颜色抖动、随机灰度、模糊、CutMix等；
+        """
+        img_w, img_s1, img_s2 = deepcopy(img), deepcopy(img), deepcopy(img)  # 创建图片副本
+        if random.random() < 0.8:  # 随机应用颜色抖动
             img_s1 = transforms.ColorJitter(0.5, 0.5, 0.5, 0.25)(img_s1)
-        img_s1 = transforms.RandomGrayscale(p=0.2)(img_s1)
-        img_s1 = blur(img_s1, p=0.5)
-        cutmix_box1 = obtain_cutmix_box(img_s1.size[0], p=0.5)
+        img_s1 = transforms.RandomGrayscale(p=0.2)(img_s1)  # 应用随机灰度变换
+        img_s1 = blur(img_s1, p=0.5)  # 应用模糊
+        cutmix_box1 = obtain_cutmix_box(img_s1.size[0], p=0.5)  # 获取CutMix盒子
 
-        if random.random() < 0.8:
+        if random.random() < 0.8:  # 同上，对第二个副本进行处理
             img_s2 = transforms.ColorJitter(0.5, 0.5, 0.5, 0.25)(img_s2)
         img_s2 = transforms.RandomGrayscale(p=0.2)(img_s2)
         img_s2 = blur(img_s2, p=0.5)
         cutmix_box2 = obtain_cutmix_box(img_s2.size[0], p=0.5)
 
-        ignore_mask = Image.fromarray(np.zeros((mask.size[1], mask.size[0])))
+        ignore_mask = Image.fromarray(np.zeros((mask.size[1], mask.size[0])))  # 创建忽略掩码
 
-        img_s1, ignore_mask = normalize(img_s1, ignore_mask)
-        img_s2 = normalize(img_s2)
+        img_s1, ignore_mask = normalize(img_s1, ignore_mask)  # 归一化第一个副本和忽略掩码
+        img_s2 = normalize(img_s2)  # 归一化第二个副本
 
-        mask = torch.from_numpy(np.array(mask)).long()
-        ignore_mask[mask == 254] = 255
+        mask = torch.from_numpy(np.array(mask)).long()  # 将掩码转换为张量
+        ignore_mask[mask == 254] = 255  # 设置忽略掩码的忽略值
 
-        return normalize(img_w), img_s1, img_s2, ignore_mask, cutmix_box1, cutmix_box2
+        return normalize(img_w), img_s1, img_s2, ignore_mask, cutmix_box1, cutmix_box2  # 返回处理后的数据集
 
     def __len__(self):
         return len(self.ids)
--- unimatch.py
+++ unimatch.py
@@ -19,13 +19,21 @@ from util.ohem import ProbOhemCrossEntropy2d
 from util.utils import count_params, init_log, AverageMeter
 from util.dist_helper import setup_distributed
 
-
-parser = argparse.ArgumentParser(description="Revisiting Weak-to-Strong Consistency in Semi-Supervised Semantic Segmentation")
+"""通过利用标记和未标记数据来处理完全监督和半监督设置。以下是其关键组件的详细分解：
+该脚本首先解析命令行参数，例如配置文件路径 ( --config )、标记和未标记数据的路径以及用于保存模型和日志的输出路径。
+然后，使用 yaml.load 函数加载配置文件，该配置文件包含模型架构、数据集和训练参数等信息。
+接着，该脚本初始化日志记录器，并将命令行参数和配置文件信息记录到日志中。
+然后，该脚本使用 torch.backends.cudnn.enabled 和 torch.backends.cudnn.benchmark 来启用 cuDNN 加速。
+然后，该脚本实例化模型、优化器和损失函数。
+接着，该脚本实例化训练集、验证集和数据加载器。
+最后，该脚本使用 torch.nn.SyncBatchNorm.convert_sync_batchnorm 函数将 BatchNorm 转换为同步 BatchNorm，并使用 torch.nn.parallel.DistributedDataParallel 将模型分布到多个 GPU 上。
+"""
+parser = argparse.ArgumentParser(description="遥感半监督实验cvpr2023")
 parser.add_argument("--config", type=str, required=True)
 parser.add_argument("--labeled-id-path", type=str, required=True)
 parser.add_argument("--unlabeled-id-path", type=str, required=True)
 parser.add_argument("--save-path", type=str, required=True)
-parser.add_argument("--local_rank", default=0, type=int)
+parser.add_argument("--local_rank", default=0, type=int)  # 分布式系统中的排名
 parser.add_argument("--port", default=None, type=int)
 
 
@@ -40,6 +48,10 @@ def main():
     rank, world_size = setup_distributed(port=args.port)
 
     if rank == 0:
+        """
+        为了确保在多个 GPU 上训练时，每个 GPU 都有自己的日志记录器和 TensorBoard 记录器，我们需要在主进程中初始化它们。
+        这会检查当前进程的等级是否为 0。在分布式训练中，每个进程都会分配一个等级。 Rank 0 通常保留用于只应执行一次而不是由每个进程执行的任务，例如记录日志和写入文件。
+        """
         all_args = {**cfg, **vars(args), "ngpus": world_size}
         logger.info("{}\n".format(pprint.pformat(all_args)))
 
@@ -50,6 +62,7 @@ def main():
     cudnn.enabled = True
     cudnn.benchmark = True
 
+    """模型实例，特别是 DeepLabV3Plus ，是根据提供的配置创建的。模型的优化器也在这里配置，骨干网和其他参数具有不同的学习率。"""
     model = DeepLabV3Plus(cfg)
     optimizer = SGD(
         [
@@ -67,11 +80,11 @@ def main():
     local_rank = int(os.environ["LOCAL_RANK"])
     model = torch.nn.SyncBatchNorm.convert_sync_batchnorm(model)
     model.cuda()
-
+    """torch.nn.parallel.DistributedDataParallel 函数将模型分布到多个 GPU 上。它需要以下参数："""
     model = torch.nn.parallel.DistributedDataParallel(
         model, device_ids=[local_rank], broadcast_buffers=False, output_device=local_rank, find_unused_parameters=False
     )
-
+    """损失函数和数据集设置。我们使用交叉熵损失函数和 ProbOhemCrossEntropy2d 损失函数。"""
     if cfg["criterion"]["name"] == "CELoss":
         criterion_l = nn.CrossEntropyLoss(**cfg["criterion"]["kwargs"]).cuda(local_rank)
     elif cfg["criterion"]["name"] == "OHEM":
@@ -79,12 +92,15 @@ def main():
     else:
         raise NotImplementedError("%s criterion is not implemented" % cfg["criterion"]["name"])
 
+    """损失函数根据配置设置。对于标记数据，使用标准或 OHEM（在线硬示例挖掘）交叉熵损失。
+    对于未标记的数据，使用不允许减少的简单交叉熵损失来单独处理每个实例。"""
     criterion_u = nn.CrossEntropyLoss(reduction="none").cuda(local_rank)
-
+    """数据集设置。我们使用 SemiDataset 类来处理半监督学习数据集。"""
     trainset_u = SemiDataset(cfg["dataset"], cfg["data_root"], "train_u", cfg["crop_size"], args.unlabeled_id_path)
     trainset_l = SemiDataset(cfg["dataset"], cfg["data_root"], "train_l", cfg["crop_size"], args.labeled_id_path, nsample=len(trainset_u.ids))
     valset = SemiDataset(cfg["dataset"], cfg["data_root"], "val")
 
+    """数据加载器设置。我们使用 torch.utils.data.DataLoader 类来加载数据集。"""
     trainsampler_l = torch.utils.data.distributed.DistributedSampler(trainset_l)
     trainloader_l = DataLoader(trainset_l, batch_size=cfg["batch_size"], pin_memory=True, num_workers=1, drop_last=True, sampler=trainsampler_l)
     trainsampler_u = torch.utils.data.distributed.DistributedSampler(trainset_u)
@@ -96,6 +112,7 @@ def main():
     previous_best = 0.0
     epoch = -1
 
+    """加载检查点。如果存在检查点，则加载模型和优化器状态。"""
     if os.path.exists(os.path.join(args.save_path, "latest.pth")):
         checkpoint = torch.load(os.path.join(args.save_path, "latest.pth"))
         model.load_state_dict(checkpoint["model"])
@@ -106,6 +123,7 @@ def main():
         if rank == 0:
             logger.info("************ Load from checkpoint at epoch %i\n" % epoch)
 
+    """训练循环。在每个 epoch 中，我们遍历标记和未标记数据加载器，并在每个迭代中计算损失并执行反向传播。"""
     for epoch in range(epoch + 1, cfg["epochs"]):
         if rank == 0:
             logger.info("===========> Epoch: {:}, LR: {:.5f}, Previous best: {:.2f}".format(epoch, optimizer.param_groups[0]["lr"], previous_best))
@@ -115,10 +133,10 @@ def main():
         total_loss_s = AverageMeter()
         total_loss_w_fp = AverageMeter()
         total_mask_ratio = AverageMeter()
-
+        """设置每个 epoch 的随机种子。这确保了每个 epoch 的数据加载器都会生成相同的随机序列。"""
         trainloader_l.sampler.set_epoch(epoch)
         trainloader_u.sampler.set_epoch(epoch)
-
+        """将标记和未标记数据加载器打包在一起。这样，我们可以在每个迭代中同时处理标记和未标记数据。"""
         loader = zip(trainloader_l, trainloader_u, trainloader_u)
 
         for i, (
@@ -126,7 +144,7 @@ def main():
             (img_u_w, img_u_s1, img_u_s2, ignore_mask, cutmix_box1, cutmix_box2),
             (img_u_w_mix, img_u_s1_mix, img_u_s2_mix, ignore_mask_mix, _, _),
         ) in enumerate(loader):
-
+            """将数据移动到 GPU 上。"""
             img_x, mask_x = img_x.cuda(), mask_x.cuda()
             img_u_w = img_u_w.cuda()
             img_u_s1, img_u_s2, ignore_mask = img_u_s1.cuda(), img_u_s2.cuda(), ignore_mask.cuda()
